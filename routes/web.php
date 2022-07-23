3eac6ba24252138aacda4cb6c3a3223e2fff8213
cweiqi27 <97137887+cweiqi27@users.noreply.github.com>
2022-07-24 07:12:05 +0800

    Add auth routes

--- routes/web.php
+++ routes/web.php
@@ -8,26 +8,26 @@ use App\Http\Controllers\LoginController;
 use App\Http\Controllers\StaffController;
 use App\Http\Controllers\RegisterController;
 
-Route::get('/', [StaffController::class, 'show']);
+Route::get('/', [StaffController::class, 'show'])->middleware('auth')->name('home');
 
 // Register
-Route::get('/register', [RegisterController::class, 'create']);
-Route::get('/register/employee', [RegisterController::class, 'createEmployee']);
-Route::get('/register/admin', [RegisterController::class, 'createAdmin']);
+Route::get('/register', [RegisterController::class, 'create'])->middleware('guest')->name('register');
+Route::get('/register/employee', [RegisterController::class, 'createEmployee'])->middleware('guest')->name('register.employee');
+Route::get('/register/admin', [RegisterController::class, 'createAdmin'])->middleware('guest')->name('register.admin');
 Route::post('/register/store/{role}', [RegisterController::class, 'store'])->name('register.store');
 
 // Email verification
 Route::get('/email/verify', 
     [AuthController::class, 'verify'])->middleware('auth')->name('verification.notice');
 Route::get('/email/verify/{id}/{hash}', 
-    [AuthController::class, 'verufyHandler'])->middleware(['auth', 'signed'])->name('verification.verify');
+    [AuthController::class, 'verifyHandler'])->middleware(['auth', 'signed'])->name('verification.verify');
 Route::post('/email/verification-notification', 
     [AuthController::class, 'resend'])->middleware(['auth', 'throttle:6,1'])->name('verification.resend');
 
 // Login
-Route::get('/login', [LoginController::class, 'login'])->name('login');
-Route::post('/authenticate', [LoginController::class, 'authenticate']);
-Route::post('/logout', [LoginController::class, 'logout']);
+Route::get('/login', [LoginController::class, 'login'])->middleware('guest')->name('login');
+Route::post('/authenticate', [LoginController::class, 'authenticate'])->middleware('guest')->name('authenticate');
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
 // Password reset
 Route::get('/forgot-password', 
