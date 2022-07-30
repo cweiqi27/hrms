972839d6773b999eb11dc78d8f003b995944ef86
cweiqi27 <97137887+cweiqi27@users.noreply.github.com>
2022-07-30 14:06:10 +0800

    Coupling Register modal

--- resources/views/components/modal/create-modal.blade.php
+++ resources/views/components/modal/create-modal.blade.php
@@ -1,5 +1,5 @@
 <div 
-    x-data="{'isModalOpen': false}" 
+    x-data="{'isModalOpen' : false}" 
     x-on:keydown.escape="isModalOpen=false"
     {{ $attributes -> merge(['class' => '']) }}>
     <p class="text-slate-700">
@@ -12,17 +12,17 @@
         </button>
     </p>
     <x-modal.modal>
-        <x-title name="Sign Up" class="text-slate-700 font-semibold self-center translate-y-[-2rem] 
+        <x-title name="REGISTER" class="text-slate-700 font-semibold tracking-wider self-center sm:translate-y-[-2rem] 
                                     underline decoration-double decoration-rose-400"/>
-        <div class="flex gap-4 sm:gap-12">
+        <div class="flex flex-col sm:flex-row sm:justify-center gap-6 sm:gap-8">
             <a href="{{ route('register.admin') }}"
-                class="p-4 rounded-lg border-2 border-emerald-600 text-slate-700 font-semibold
+                class="p-4 rounded-lg border-2 border-emerald-600 text-slate-700 text-center font-semibold
                 hover:bg-emerald-600 hover:text-slate-50 hover:outline 
                 hover:outline-rose-300 outline-offset-0 transition-all">
                 Admin
             </a>
             <a href="{{ route('register.employee') }}"
-                class="p-4 rounded-lg border-2 border-emerald-600 text-slate-700 font-semibold
+                class="p-4 rounded-lg border-2 border-emerald-600 text-slate-700 text-center font-semibold
                 hover:bg-emerald-600 hover:text-slate-50 hover:outline 
                 hover:outline-rose-300 outline-offset-0 transition-all">
                 Employee
--- resources/views/components/modal/modal.blade.php
+++ resources/views/components/modal/modal.blade.php
@@ -1,18 +1,19 @@
 <div 
-    class="absolute top-0 left-0 w-screen h-screen backdrop-blur-3xl backdrop-brightness-90"
+    class="z-50 fixed top-0 left-0 w-screen h-full backdrop-blur-2xl backdrop-brightness-75"
     x-show="isModalOpen"
     x-cloak
-    x-transition
+    x-transition.opacity
+    x-transition:leave.duration.150ms
 >
     <div 
         class="flex justify-center items-center w-full h-full"
         x-show="isModalOpen"
-        x-on:click.away="isModalOpen = false"
         x-cloak
         x-transition
+        x-transition:leave.duration.150ms
     >
         <div {{ $attributes -> merge([
-            'class' => 'flex flex-col gap-4 rounded-md bg-white p-8 shadow-md'
+            'class' => 'flex flex-col gap-4 min-w-min rounded-md bg-white pt-4 pb-6 px-8 shadow-md'
             ]) 
         }}>
         <button x-on:click="isModalOpen = false" class="relative flex-none self-end">
