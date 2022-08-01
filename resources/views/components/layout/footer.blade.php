60d07e048a79071a374c4952edf00536ef724e1e
cweiqi27 <97137887+cweiqi27@users.noreply.github.com>
2022-08-02 06:34:01 +0800

    Add footer content and new layout component

--- resources/views/components/layout/footer.blade.php
+++ resources/views/components/layout/footer.blade.php
@@ -1,19 +1,24 @@
 <footer class="absolute bottom-0 w-full p-4 bg-slate-800">
-    <nav class="flex flex-col sm:flex-row justify-around gap-4">
-        <div>
-            <h2 class="font-bold text-slate-50">Title</h1>
-            <p class="text-slate-300">something here</p>
-            <p class="text-slate-300">something here</p>
+    <nav class="flex flex-col sm:flex-row justify-evenly gap-4">
+        <div class="grow-0">
+            <h2 class="font-bold text-slate-50">HRMS</h1>
+            <p class="text-slate-300">Human Resource Management System</p>
         </div>
-        <div>
-            <h2 class="font-bold text-slate-50">Title</h1>
-            <p class="text-slate-300">something here</p>
-            <p class="text-slate-300">something here</p>
+        <div class="grow-0">
+            <h2 class="font-bold text-slate-50">Motto</h1>
+            <p class="text-slate-300">
+                Real human bean
+                and a real hero
+            </p>
+                
         </div>
-        <div>
-            <h2 class="font-bold text-slate-50">Title</h1>
-            <p class="text-slate-300">something here</p>
-            <p class="text-slate-300">something here</p>
+        <div class="grow-0">
+            <h2 class="font-bold text-slate-50">Purpose</h1>
+            <p class="text-slate-300">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Alias iusto eaque libero nam nisi nostrum. 
+            </p>
         </div>
+        
     </nav>
 </footer>
