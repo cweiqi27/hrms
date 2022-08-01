de3d636695a4397c379e625ee390ce4bb6206cb7
cweiqi27 <97137887+cweiqi27@users.noreply.github.com>
2022-08-02 06:32:34 +0800

    Abstractify components

--- resources/views/components/layout/sidebar.blade.php
+++ resources/views/components/layout/sidebar.blade.php
@@ -1,16 +1,17 @@
+@props(['linksCsv'])
+
+@php
+    if(isset($linksCsv)) $link = explode(',', $linksCsv);    
+@endphp
+
+@if(isset($linksCsv))
 <aside class="hidden lg:block fixed inset-0 top-[3.53rem] right-auto w-44 px-4 bg-zinc-100">
     <ul class="flex flex-col shrink-0 gap-2 my-4">
-        <li class="px-4 py-2 font-medium text-slate-700 hover:cursor-pointer hover:text-emerald-500 hover:translate-x-1 ease-in-out duration-150">
-            <a href="">placehodler</a>
-        </li>
-        <li class="px-4 py-2 font-medium text-slate-700 hover:cursor-pointer hover:text-emerald-500 hover:translate-x-1 ease-in-out duration-150">
-            <a href="">placehodler</a>
-        </li>
-        <li class="px-4 py-2 font-medium text-slate-700 hover:cursor-pointer hover:text-emerald-500 hover:translate-x-1 ease-in-out duration-150">
-            <a href="">placehodler</a>
-        </li>
-        <li class="px-4 py-2 font-medium text-slate-700 hover:cursor-pointer hover:text-emerald-500 hover:translate-x-1 ease-in-out duration-150">
-            <a href="">placehodler</a>
-        </li>
+        @foreach ($link as $links)    
+            <li class="px-4 py-2 font-medium text-slate-700 hover:cursor-pointer hover:text-emerald-500 hover:translate-x-1 ease-in-out duration-150">
+                <a href="">{{ $links }}</a>
+            </li>
+        @endforeach
     </ul>
 </aside>
+@endif
--- resources/views/components/table.blade.php
+++ resources/views/components/table.blade.php
@@ -1,30 +1,20 @@
-@props(['leaves'])
+@props(['titleCsv'])
+
+@php
+    $title = explode(',', $titleCsv);
+@endphp
 
 <div class="overflow-x-auto m-5">
     <table class="table-auto border border-separate border-slate-400">
         <thead class="bg-slate-100 text-slate-600">
             <tr>
-                <th class="p-3 border border-slate-300">{{$leaves->leave_id}}</th>
-                <th class="p-3 border border-slate-300">{{$leaves->leave_start_date}}</th>
-                <th class="p-3 border border-slate-300">{{$leaves->employee_id}}</th>
+                @foreach ($title as $titles)
+                    <th class="p-3 border border-slate-300">{{ $titles }}</th>
+                @endforeach
             </tr>
         </thead>
         <tbody class="text-slate-600">
-            <tr>
-                <td class="p-3 border border-slate-300">Test 🤓</th>
-                <td class="p-3 border border-slate-300">Test 🤓</th>
-                <td class="p-3 border border-slate-300">Test 🤓</th>
-            </tr>
-            <tr>
-                <td class="p-3 border border-slate-300">Test 🤓</th>
-                <td class="p-3 border border-slate-300">Test 🤓</th>
-                <td class="p-3 border border-slate-300">Test 🤓</th>
-            </tr>
-            <tr>
-                <td class="p-3 border border-slate-300">Test 🤓</th>
-                <td class="p-3 border border-slate-300">Test 🤓</th>
-                <td class="p-3 border border-slate-300">Test 🤓</th>
-            </tr>
+            {{ $slot }}
         </tbody>
     </table>
 </div>
