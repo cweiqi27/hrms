1d8c5906e69f8b990281efe01987f4024623d98b
cweiqi27 <97137887+cweiqi27@users.noreply.github.com>
2022-08-01 20:59:13 +0800

    Change business hours

--- config/carbon.php
+++ config/carbon.php
@@ -1,25 +1,23 @@
 <?php return [
     'opening-hours' => [
-        'monday' => ['09:00-12:00', '13:00-18:00'],
-        'tuesday' => ['09:00-12:00', '13:00-18:00'],
-        'wednesday' => ['09:00-12:00'],
-        'thursday' => ['09:00-12:00', '13:00-18:00'],
-        'friday' => ['09:00-12:00', '13:00-20:00'],
-        'saturday' => ['09:00-12:00', '13:00-16:00'],
+        'monday' => ['09:00-12:00', '13:00-17:00'],
+        'tuesday' => ['09:00-12:00', '13:00-17:00'],
+        'wednesday' => ['09:00-12:00', '13:00-17:00'],
+        'thursday' => ['09:00-12:00', '13:00-17:00'],
+        'friday' => ['09:00-12:00', '13:00-17:00'],
+        'saturday' => [],
         'sunday' => [],
         'exceptions' => [
-        '2016-11-11' => ['09:00-12:00'],
-        '2016-12-25' => [],
-        '01-01' => [], // Recurring on each 1st of january
-        '12-25' => ['09:00-12:00'], // Recurring on each 25th of december
+            '01-01' => [], // Recurring on each 1st of january
+            '12-25' => [], // Recurring on each 25th of december
         ],
     ],
     'holidaysAreClosed' => true,
     'holidays' => [
         'region' => 'asia/kuala_lumpur',
-        'with' => [
-        'boss-birthday' => '09-26',
-        'last-monday'   => '= last Monday of October',
-        ],
+        // 'with' => [
+        // 'boss-birthday' => '09-26',
+        // 'last-monday'   => '= last Monday of October',
+        // ],
     ],
 ];
