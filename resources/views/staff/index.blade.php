0dd525fcbfcab8c8d2126458b773cb1a11b599e8
cweiqi27 <97137887+cweiqi27@users.noreply.github.com>
2022-08-07 19:30:23 +0800

    Fix home page route, home page greet

--- resources/views/staff/index.blade.php
+++ resources/views/staff/index.blade.php
@@ -3,9 +3,9 @@
     <x-title :name="$message"/>
     <x-subheading>
         @if($is_closed)
-            Work ends in {{ $diff_next }} ({{ $next_open_or_close }} p.m.).
-        @else
             Work starts in {{ $diff_next  }} ({{ $next_open_or_close }} a.m.).
+        @else
+            Work ends in {{ $diff_next }} ({{ $next_open_or_close }} p.m.).
         @endif
     </x-subheading>
     {{ $time_now }}
