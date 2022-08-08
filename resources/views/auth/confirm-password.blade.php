1e5b90d19d82696910208204da6b99c720d9b1fa
cweiqi27 <97137887+cweiqi27@users.noreply.github.com>
2022-08-08 23:11:16 +0800

    Clarify confirm password warning

--- resources/views/auth/confirm-password.blade.php
+++ resources/views/auth/confirm-password.blade.php
@@ -1,13 +1,16 @@
 <x-layout.main-layout title="Confirm password" type="auth">
-    <x-layout.form-layout class="flex-col px-2 sm:px-12 items-start gap-8 h-[34rem]">
+    <x-layout.form-layout class="flex-col px-2 sm:px-12 items-start gap-12 h-[34rem]">
         <div class="flex gap-4 items-end">
             <x-title name="Hang on" />
-            <ion-icon name="alert-circle-outline" class="text-3xl text-yellow-500"></ion-icon>
+            <ion-icon name="alert-circle-outline" class="text-3xl text-yellow-500 pointer-events-none"></ion-icon>
         </div>
-        <x-subheading>We have to make sure it's really you before you can proceed to this page.</x-subheading>
+        <div class="flex flex-col gap-4">
+            <x-subheading>You're entering sensitive territory.</x-subheading>
+            <x-subheading>We just have to really make sure it's actually you.</x-subheading>
+        </div>
         <form action="{{ route('password.confirm-password') }}" method="POST" autocomplete="off">
             @csrf
-            <x-form.input name="password" labelName="Password" type="password"/>
+            <x-form.input name="password" labelName="Your password" type="password"/>
             <x-button.form-submit>Confirm</x-button.form-submit>
         </form>
     </x-layout.form-layout>
