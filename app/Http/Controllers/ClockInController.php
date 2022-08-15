0e0d483636048aeec14be472e7271f1e3289dcf9
cweiqi27 <97137887+cweiqi27@users.noreply.github.com>
2022-08-16 00:34:52 +0800

    Add clock in feature

--- app/Http/Controllers/ClockInController.php
+++ app/Http/Controllers/ClockInController.php
@@ -2,16 +2,59 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Attendance;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Auth;
 
 class ClockInController extends Controller
 {
     public function clockIn(Request $request)
     {
+        $diff_hour_next = Carbon::parse(Carbon::now())->diffInRealHours(Carbon::nextOpen());
+
+        if(! $request->session()->exists('clock-in') && Carbon::isBusinessOpen()) {
+            $request->session()->put('clock-in', Carbon::now()->toDateTimeString());
+            return back()
+                ->with([
+                    'staff' => Auth::user()->staff_id,
+                    'message' => 'You clocked-in.'
+                ]);
+        } else {
+            return back()
+                ->with([
+                    'staff' => Auth::user()->staff_id,
+                    'message' => 'You have ' . $diff_hour_next . ' hours left before you can clock in.'
+                ]);
+        }
+
     }
 
-    public function clockOut()
+    public function clockOut(Request $request)
     {
+        $diff_hour_next = Carbon::parse(Carbon::now())->diffInRealHours(Carbon::nextClose());
+
+        if($request->session()->has('clock-in') && Carbon::isBusinessClosed()) {
+
+            $request->session()->put('clock-out', Carbon::now()->toDateTimeString());
+            Attendance::create([
+                'date' => Carbon::now()->toDateString(),
+                'clock_in_time' => $request->session()->get('clock-in'),
+                'clock_out_time' => $request->session()->get('clock-out'),
+                'staff_id' => Auth::user()->staff_id
+            ]);
+
+            return back()
+                ->with([
+                    'staff' => Auth::user()->staff_id,
+                    'message' => 'You clocked-out.'
+                ]);
+        } else {
+            return back()
+                ->with([
+                    'staff' => Auth::user()->staff_id,
+                    'message' => 'You have ' . $diff_hour_next . ' hours left before you can clock out.'
+                ]);
+        }
     }
 }
--- resources/views/components/layout/header-mobile.blade.php
+++ resources/views/components/layout/header-mobile.blade.php
@@ -18,7 +18,8 @@
 
 {{--Dropdown--}}
 <div
-    class="flex-none fixed top-[3.53rem] left-0 w-screen h-screen py-4 flex flex-col items-center gap-2 md:hidden bg-emerald-800"
+    class="flex-none fixed top-[3.53rem] left-0 w-screen h-screen
+            flex flex-col items-center md:hidden bg-emerald-800"
     x-show="isDropdownOpen"
     x-cloak
     x-transition:enter="transition ease-out duration-300"
@@ -28,15 +29,48 @@
     x-transition:leave-start="translate-x-0"
     x-transition:leave-end="translate-x-full"
 >
+    @unless($role === 'admin')
+        @can('clock-in')
+            <form
+                action="{{ route('clockin') }}"
+                method="POST"
+            >
+                @csrf
+                <button
+                    class="w-full py-4 text-slate-50 text-center font-semibold
+                        border-b-2 border-emerald-600 border-dotted"
+                >
+                    Clock In
+                </button>
+            </form>
+        @endcan
+        @cannot('clock-in')
+            <form
+                action="{{ route('clockout') }}"
+                method="POST"
+            >
+                @csrf
+                <button
+                    class="w-full py-4 text-slate-50 text-center font-semibold
+                    border-b-2 border-emerald-600 border-dotted"
+                >
+                    Clock In
+                </button>
+            </form>
+        @endcannot
+    @endunless
 
-    <a href="{{ route('home') }}" class="w-full py-2 text-slate-50 text-center font-semibold">Home</a>
-    <a href="" class="w-full py-2 text-slate-50 text-center font-semibold">Task</a>
-    <a href="{{ route('leave.show') }}" class="w-full py-2 text-slate-50 text-center font-semibold">Leave</a>
-    <a href="{{ route('monitor.show') }}" class="w-full py-2 text-slate-50 text-center font-semibold">Monitor</a>
-    <a href="{{ route('staff.profile') }}" class="w-full py-2 text-slate-50 text-center font-semibold">Profile</a>
-    <form class="inline" method="POST" action="/logout" class="w-full text-center">
+    <a href="{{ route('home') }}" class="w-full py-4 text-slate-50 text-center font-semibold border-b-2 border-emerald-600 border-dotted">Home</a>
+
+    <a href="" class="w-full py-4 text-slate-50 text-center font-semibold border-b-2 border-emerald-600 border-dotted">Task</a>
+    <a href="{{ route('leave.show') }}" class="w-full py-4 text-slate-50 text-center font-semibold border-b-2 border-emerald-600 border-dotted">Leave</a>
+    <a href="{{ route('monitor.show') }}" class="w-full py-4 text-slate-50 text-center font-semibold border-b-2 border-emerald-600 border-dotted">Monitor</a>
+    <a href="{{ route('staff.profile') }}" class="w-full py-4 text-slate-50 text-center font-semibold border-b-2 border-emerald-600 border-dotted outline-">Profile</a>
+
+    <form class="inline" method="POST" action="/logout">
         @csrf
-        <x-button.submit class="py-2 text-slate-50 font-semibold">
+        <x-button.submit class="rounded-full mt-4 p-4 bg-rose-200 border-8 border-double border-rose-50
+                                text-gray-700 font-bold">
             Sign out
         </x-button.submit>
     </form>
