8f77ec44bf4ab812e3fb15c0d374913b98664130
cweiqi27 <97137887+cweiqi27@users.noreply.github.com>
2022-08-26 04:51:31 +0800

    Remove db seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,10 +14,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(10)->create();
 
-        \App\Models\Employee::factory(10)->create();
-
-        \App\Models\Leave::factory(10)->create();
     }
 }
