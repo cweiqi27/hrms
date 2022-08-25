2f3a6b10f9afa848bbf43db551c2ebb0e0d26ada
cweiqi27 <97137887+cweiqi27@users.noreply.github.com>
2022-08-26 04:51:48 +0800

    Add task index page

--- resources/views/task/index.blade.php
+++ resources/views/task/index.blade.php
@@ -1,4 +1,43 @@
-<x-layout.main-layout title="Task" type="dashboard" :role="$staff->role" sidebarLinkType="task">
-    <x-title name="Task" />
-    <a href="{{ route('task.create') }}">create</a>
+@php
+    $staff->role === 'admin'
+        ? $subheading = "Assign task appropriately according to employee's strengths."
+        : $subheading = "Perform the assigned tasks with all your might."
+@endphp
+
+<x-layout.main-layout title="Task" type="index" :role="$staff->role" sidebarLinkType="task">
+    <x-layout.index-layout>
+        <x-layout.index-heading
+            heading="Task"
+            :subheading="$subheading"
+        />
+        <x-layout.index-main>
+            @if($staff->role === 'admin')
+                <x-card.nav-card
+                    route="task.create"
+                    title="Create Task"
+                    description="Assign task to employee"
+                    icon="clipboard-outline"
+                />
+                <x-card.nav-card
+                    route="task.list"
+                    title="Review"
+                    description="Review assigned task"
+                    icon="checkbox-outline"
+                />
+            @else
+                <x-card.nav-card
+                    route="task.create"
+                    title="Task List"
+                    description="Create task for employees to perform."
+                    icon="search-circle-outline"
+                />
+                <x-card.nav-card
+                    route="task.show"
+                    title="Create Task"
+                    description="Create task for employees to perform."
+                    icon="search-circle-outline"
+                />
+            @endif
+        </x-layout.index-main>
+    </x-layout.index-layout>
 </x-layout.main-layout>
