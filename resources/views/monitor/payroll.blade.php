e23dc25c128dc25474b9b08cccea2044ab376428
cweiqi27 <97137887+cweiqi27@users.noreply.github.com>
2022-09-01 22:31:08 +0800

    Add payroll page

---
+++ resources/views/monitor/payroll.blade.php
@@ -0,0 +1,5 @@
+<x-layout.main-layout title="Payroll" type="dashboard" :role="$staff->role" sidebarLinkType="monitor">
+    <main class="flex flex-col gap-4 mx-auto">
+
+    </main>
+</x-layout.main-layout>
