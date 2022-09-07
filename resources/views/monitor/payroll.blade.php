83101131e748677af73a8d07edeaf77ebef3f10f
cweiqi27 <97137887+cweiqi27@users.noreply.github.com>
2022-09-07 21:01:32 +0800

    Disable admin field if not assigned employee

--- resources/views/monitor/payroll.blade.php
+++ resources/views/monitor/payroll.blade.php
@@ -1,5 +1,22 @@
 <x-layout.main-layout title="Payroll" type="dashboard" :role="$staff->role" sidebarLinkType="monitor">
-    <main class="flex flex-col gap-4 mx-auto">
-
+    <main class="flex flex-col gap-4">
+        <section class="flex flex-col gap-4 mt-10 px-2 sm:px-6">
+            <form action="{{ route('task.list-get', ['staff' => 'staff_name']) }}" method="GET">
+                <div class="flex sm:w-[60rem] mx-auto">
+                    <x-form.select name="employee" label-name="">
+                        @foreach($staff_list as $staff_lists)
+                            <option value="{{$staff_lists->staff_id}}">{{ $staff_lists->name }}</option>
+                        @endforeach
+                    </x-form.select>
+                    <x-button.submit
+                        class="rounded-r-lg px-4
+                            bg-emerald-500 hover:bg-emerald-400
+                            text-slate-50"
+                    >
+                        <ion-icon name="search-outline" />
+                    </x-button.submit>
+                </div>
+            </form>
+        </section>
     </main>
 </x-layout.main-layout>
--- resources/views/task/create.blade.php
+++ resources/views/task/create.blade.php
@@ -4,13 +4,17 @@
         <div class="mt-8">
             <form action="{{ route('task.store')  }}" method="POST" autocomplete="off">
                 @csrf
-                <x-form.select labelName="Employee" name="staff_id">
-                    @foreach($employees as $employee)
-                        <option value="{{ $employee->staff_id }}">{{ $employee->name }}</option>
-                    @endforeach
-                </x-form.select>
-                <x-form.input labelName="Task name" name="task_name" />
-                <x-button.form-submit>Create</x-button.form-submit>
+                @if(count($managed_staff) === 0)
+                    <h2 class="text-slate-600 text-xl">No employee assigned to you at the moment.</h2>
+                @else
+                    <x-form.select labelName="Employee" name="staff_id">
+                        @foreach($managed_staff as $managed_staffs)
+                            <option value="{{ $managed_staffs->staff_id }}">{{ $managed_staffs->name }}</option>
+                        @endforeach
+                    </x-form.select>
+                    <x-form.input labelName="Task name" name="task_name" />
+                    <x-button.form-submit>Create</x-button.form-submit>
+                @endif
             </form>
         </div>
     </section>
