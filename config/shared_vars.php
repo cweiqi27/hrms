e47a1b8998bc5ca017e8e77459ca289558959c1d
cweiqi27 <97137887+cweiqi27@users.noreply.github.com>
2022-09-14 23:54:36 +0800

    Add positions vars

--- config/shared_vars.php
+++ config/shared_vars.php
@@ -18,14 +18,45 @@ return [
         "0" => "task.create",
         "1" => "task.list",
     ],
-    'taskLinksCsv-employee' => 'Create,Review',
+    'taskLinksCsv-employee' => 'Task List',
     'taskLinksHref-employee' => [
-        "0" => "task.create",
-        "1" => "monitor.show",
+        "0" => "task.list"
     ],
     'monitorLinksCsv-employee' => 'Performance,Payroll',
     'monitorLinksHref-employee' => [
         "0" => "task.create",
         "1" => "task.show",
     ],
+    'itPositions' => [
+        "Backend Developer" => "3200",
+        "Data Scientist" => "3400",
+        "DevOps Engineer" => "3600",
+        "Frontend Developer" => "3000",
+        "IT Support" => "2800",
+        "Mobile Developer" => "3200",
+        "Product Manager" => "4000",
+        "Tech Lead" => "6000",
+        "UI/UX Designer" => "3000",
+    ],
+    'financePositions' => [
+        "Accountant" => "3500",
+        "Auditor" => "3200",
+        "Financial Analyst" => "3500",
+        "Financial Controller" => "3300",
+        "Portfolio Manager" => "4000",
+    ],
+    'hrPositions' => [
+        "HR Coordinator" => "3800",
+        "HR Director" => "4800",
+        "HRIS" => "3400",
+        "HR Manager" => "4000",
+        "Recruiter" => "3000",
+    ],
+    'marketingPositions' => [
+        "Communications Manager" => "3500",
+        "Content Marketing Specialist" => "3000",
+        "Creative Director" => "5000",
+        "Digital Marketing Manager" => "3500",
+        "Marketing Analyst" => "3000",
+    ]
 ];
